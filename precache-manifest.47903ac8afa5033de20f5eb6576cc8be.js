e7dcc50ce605a93480cb1ca051bdbdee7f36237c
Arun Bagga <abarunbagga12@gmail.com>
2020-07-03 19:12:33 +0530

    Updates

---
+++ precache-manifest.47903ac8afa5033de20f5eb6576cc8be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd73382264dd5289cee74a9a17b3010e",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "1877cbbf597222a746e4",
+    "url": "/reactwebsite/static/css/2.1c690216.chunk.css"
+  },
+  {
+    "revision": "8d2381f0d05a750f1c9a",
+    "url": "/reactwebsite/static/css/main.e3864a8f.chunk.css"
+  },
+  {
+    "revision": "1877cbbf597222a746e4",
+    "url": "/reactwebsite/static/js/2.bae20c1a.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/reactwebsite/static/js/2.bae20c1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2381f0d05a750f1c9a",
+    "url": "/reactwebsite/static/js/main.6ae86e75.chunk.js"
+  },
+  {
+    "revision": "e44a92314b55085d6ec6",
+    "url": "/reactwebsite/static/js/runtime-main.af302b26.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactwebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactwebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactwebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactwebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactwebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
